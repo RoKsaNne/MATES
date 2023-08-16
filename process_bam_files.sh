8c344ddd3473eb48877ebf37143b124d39affca1
Ruohan Wang <ruohan.wang4@mail.mcgill.ca>
2023-08-16 10:27:57 -0400

    Add pipeline required python files

--- process_bam_files.sh
+++ process_bam_files.sh
@@ -3,10 +3,11 @@
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -t threads_num -f file_name -p path_to_bam"
+   echo "Usage: $0 -t threads_num -f file_name -p path_to_bam -m data_mode"
    echo -e "\t-t Threads number"
    echo -e "\t-f File contains sample name"
    echo -e "\t-p Path to STAR/STAR_Solo aligned bam folder"
+   echo -e "\t-m 10X or Smart-seq2"
    exit 1 # Exit script after printing help
 }
 
@@ -16,6 +17,7 @@ do
       t ) threads_num="$OPTARG" ;;
       f ) file_name="$OPTARG" ;;
       p ) path_to_bam="$OPTARG" ;;
+      m ) data_mode="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
@@ -57,10 +59,33 @@ for split_file in ./file_tmp/*; do
     count=$((count + 1))
 done
 
+
 ####### Split bam files into unique reads bam files and multi reads bam files ########
+echo Start splitting bam files into unique/multi reads sub-bam files ...
 for ((i=0; i <= file_batch; i++));
     do 
         sh helper_sh/split_u_m.sh ./file_tmp/${i}  $path_to_bam &
     done
     wait
+
+echo Finish splitting bam files into unique reads and multi reads sub-bam files.
+
+
+echo Start splitting unique sub-bam based on cell barcodes...
+for ((i=0; i <= file_batch; i++));
+    do 
+        sh helper_sh/split_bc_u.sh ./file_tmp/${i}  $path_to_bam &
+    done
+    wait
+echo Finish splitting unique sub-bam.
+
+echo Start splitting multi sub-bam based on cell barcodes...
+for ((i=0; i <= file_batch; i++));
+    do 
+        sh helper_sh/split_bc_m.sh ./file_tmp/${i}  $path_to_bam &
+    done
+    wait
+echo Finish splitting multi sub-bam.
+
+
 
